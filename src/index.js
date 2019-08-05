5b80fc8f11c0a059a852bf542419b078d2670730
Kristopher Green <kristopher_e_green@homedepot.com>
2019-08-05 08:09:02 -0400

    Setup initial store, actions, and reducer

--- src/index.js
+++ src/index.js
@@ -1,10 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createStore } from "redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ACTION => DESCRIBES WHAT WE WANT TO DO | RETURNS AN OBJ
+const icrementAction = () => {
+  return {
+    type: "INCREMENT"
+  };
+};
+
+const decrementAction = () => {
+  return {
+    type: "DECREMENT"
+  };
+};
+
+//REDUCER => USES 'ACTION' TO TRANSFORMS STATE
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    // no default
+  }
+  return state;
+};
+
+//STORE => GLOBALIZED STATE
+let store = createStore(counterReducer);
+
+//Display state from store
+store.subscribe(() => console.log(store.getState()));
+
+//DISPATCH => SEND OUR 'ACTION' TO THE 'REDUCER'
+store.dispatch(icrementAction());
+store.dispatch(decrementAction());
+store.dispatch(icrementAction());
+store.dispatch(icrementAction());
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
